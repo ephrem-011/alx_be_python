637a3e8807914be41f2dfcdc0248d0229fac1be2
Ephrem <ephraimfekadu@gmail.com>
2025-01-05 15:12:47 +0300

    Delete control-flow directory

--- control-flow/pattern_drawing.py
+++
@@ -1,8 +0,0 @@
-length=int(input("Enter the size of the pattern: "))
-count=length
-while (count!=0):
-    for j in range(0, length):
-        for i in range (0,length):
-            print("*", end="")
-        count=count-1
-        print ("\n")
