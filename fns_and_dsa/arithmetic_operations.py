3aa6ca2b9a6ff570d143b03f0306a5b0acc60665
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-08 14:54:23 +0300

    list

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,12 +6,7 @@ def perform_operation(num1, num2, operation):
         elif (operation=="multiply"):
             return num1*num2
         elif (operation=="divide"):
-<<<<<<< HEAD
-            if num2==0:
-                return "Division by zero not possible"
-=======
             if num2 == 0:
                 print("Division by zero iss not possible")
->>>>>>> main
             else:
                 return num1/num2
