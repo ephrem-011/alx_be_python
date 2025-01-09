d5605a7905a4afe8b9edf9950b5d8c6daeea3308
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-09 14:51:30 +0300

    shopping

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,14 +14,14 @@ def main():
             item=str(input("Enter the item to add: ")).lower() # Prompt for and add an item
             shopping_list.append(item)
         elif choice == '2':
+            print("Enter the item to remove: ",end="")
             while True:
-                item2=str(input("Enter the item to remove: ")).lower() # Prompt for and remove an item
+                item2=str(input()).lower() # Prompt for and remove an item
                 if item2 in shopping_list:
                     shopping_list.remove(item2)
                     break
                 else:
-                    print ("This item is not in the shopping list. Please enter the name correctly and try again")
-                    continue
+                    print ("This item is not in the shopping list. Please enter the name correctly and try again. ", end="")
         elif choice == '3':
             print (shopping_list)# Display the shopping list
         elif choice == '4':
