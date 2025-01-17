65399b4d8fcf5700d312d68c2cf32b11d297eae6
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-17 14:19:25 +0300

    cls

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,4 +8,4 @@ class BankAccount:
     def withdraw(self, amount):
         self.amount=amount
     def display_balance(self):
-        print (f"Current Balance: ${round(self.account_balance, 2)}")
+        print (f"Current Balance: ${round(float(self.account_balance), 2)}")
