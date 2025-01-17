a869ba23e40f31cc398f145a71c0546550866633
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-17 17:52:49 +0300

    div

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,9 @@
 def safe_divide(numerator, denominator):
     try:
-        result=numerator / denominator
-    except ZeroDivisionError as e:
-        print (e)
+        result=float(numerator) / float(denominator)
     except ValueError as v:
         print (v)
+    except ZeroDivisionError as e:
+        print (e)
     else:
         return f"The result is {result}"
