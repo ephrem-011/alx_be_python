7302829c3a300cf944e7771aa0caae4affaed24a
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-18 13:25:02 +0300

    test

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+class Library:
+    def __init__(self, _books: Book):
+        self._books[:]=_books
+    def add_book (self, book(title, author)):
+        self.book(title, author)=book
+    def checkout_book(self, title):
+        self.title = title
+    def return_book(self, title):
+        self.title = title
