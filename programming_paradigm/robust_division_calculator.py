f736ff9eec9262ab047aeac1d6082cc53dfa4015
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-18 11:35:35 +0300

    test

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,9 +2,9 @@ def safe_divide(numerator, denominator):
     try:
         numerator=float(numerator)
         denominator=float(denominator)
-        return f"The result of the division is {numerator / denominator}"
+        print (f"The result of the division is {numerator / denominator}")
     except ValueError:
-        return f"Error: Please enter numeric values only." 
+        print( f"Error: Please enter numeric values only." )
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero."
+        print ("Error: Cannot divide by zero.")
    
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from simple_calculator import SimpleCalculator
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        # Add more assertions to thoroughly test the add method.
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10,5), 5)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(10,5), 50)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(9,3), 3)
+        self.assertEqual(self.calc.divide(5,0), None)
+       
