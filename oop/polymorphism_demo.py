1365114c389863694026ea4fbd82029a60083d6d
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-25 15:01:26 +0300

    oop

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,12 +1,12 @@
-class shape:
+class Shape:
     def area(self):
         raise NotImplementedError
-class circle(shape):
+class Circle(Shape):
     def __init__(self, radius):
         self.radius=radius
     def area(self):
         return 3.14*self.radius*self.radius
-class rectangle(shape):
+class Rectangle(Shape):
     def __init__(self, length, width):
         self.length =  length
         self.width = width
