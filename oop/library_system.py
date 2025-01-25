29dd7f8a4ea43695ae3432974da15e7c8a819ed5
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-25 14:51:25 +0300

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,14 +2,20 @@ class Book:
     def __init__(self, title: str, author: str):
         self.title = title 
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size=file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count=page_count
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     def __init__(self):
         self.books = []
@@ -19,10 +25,4 @@ class Library:
         self.books.append(book)
     def list_books(self):
         for i in self.books:
-            if i.__class__.__name__ == "Book":
-                print(f"Book: {i.title} by {i.author}")
-            elif i.__class__.__name__ == "EBook":
-                print(f"EBook: {i.title} by {i.author}, File Size: {i.file_size}KB")
-            elif i.__class__.__name__ == "PrintBook":
-                print(f"PrintBook: {i.title} by {i.author}, Page Count: {i.page_count}")
-            #print (f"{i.__class__.__name__}: {i.title} by {i.author}")
+            print(i)
