d38bb5798b5c2e12b6703834df4b155df079444e
eph-rem007 <ephraimfekadu@gmail.com>
2025-01-25 14:37:35 +0300

    oop

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title 
+        self.author = author
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size=file_size
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count=page_count
+class Library:
+    books=[]
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for i in self.books:
+            if i.__class__.__name__ == "Book":
+                print(f"Book: {i.title} by {i.author}")
+            elif i.__class__.__name__ == "EBook":
+                print(f"EBook: {i.title} by {i.author}, File Size: {i.file_size}KB")
+            elif i.__class__.__name__ == "PrintBook":
+                print(f"PrintBook: {i.title} by {i.author}, Page Count: {i.page_count}")
+            #print (f"{i.__class__.__name__}: {i.title} by {i.author}")
